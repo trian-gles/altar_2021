934d8d01c0ff998485f5c9593e1ef95a568f5eca
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-05-03 13:27:16 -0400

    Initial commit

---
+++ audio.py
@@ -0,0 +1,22 @@
+from pyo import *
+
+
+class SynthManager:
+    def input(self, msg):
+        pass
+
+
+class Zone:
+    pass
+
+
+class ZoneOne(Zone):
+    pass
+
+
+class ZoneTwo(Zone):
+    pass
+
+
+class ZoneThree(Zone):
+    pass

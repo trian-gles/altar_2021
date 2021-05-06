61bcf52c92566d4f745a3ef95a749d0c6495415a
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-05-05 22:27:24 -0400

    added flexible module routing

--- dx7.py
+++ dx7.py
@@ -9,27 +9,23 @@ s = Server().boot()
 class DXSineModule:
     env = None
 
-    def __init__(self, ratio=1.0):
+    def __init__(self, ratio=1.0, level=1.0):
         self.ratio = Sig(ratio)
         self.ratio.ctrl([SLMap(0, 8.0, 'lin', 'value', ratio)])
         self.phasor = Phasor(200, mul=math.pi*2)
-
+        self.level = Sig(level)
         self.cos = Cos(input=self.phasor, mul=self.env)
+        self.output = self.level + self.cos
         self.calldecay = None
 
     def modulate_phase(self, modulating_sig):
         self.cos.input += modulating_sig
 
-    def modulate_sine(self, mod_sine):
-        self.scaling = Sig(1)
-        mod_sine.modulate_phase(self.cos * self.scaling)
-        self.scaling.ctrl([SLMap(0, 8, 'lin', 'value', 1)], title="Scaling")
-
     def change_pitch(self, freq):
         self.phasor.freq = freq * self.ratio
 
-    def out(self):
-        self.mixed = self.cos.mix(2) * 0.3
+    def out(self, level):
+        self.mixed = self.output.mix(2) * level * 0.3
         self.mixed.out()
 
 
@@ -46,14 +42,33 @@ class DX7:
         self.module_6 = DXSineModule(ratio=.5)
         self.all_mods = (self.module_1, self.module_2, self.module_4, self.module_3, self.module_5, self.module_6)
 
-        self.module_2.modulate_sine(self.module_1)
-        self.module_1.out()
+        self.routes = {}
+        in_mod_count = 1
+        for modding in self.all_mods:
+            out_mod_count = 1
+            for modded in self.all_mods:
+                ctrl_sig = Sig(0)
+                title = f"{in_mod_count}{out_mod_count}"
+                self.routes[title] = ctrl_sig
 
-        self.module_6.modulate_sine(self.module_6)
-        self.module_6.modulate_sine(self.module_5)
-        self.module_5.modulate_sine(self.module_4)
-        self.module_4.modulate_sine(self.module_3)
-        self.module_3.out()
+                #ctrl_sig.ctrl([SLMap(0, 8.0, 'lin', 'value', 0)], title=title + " route control")
+
+                if in_mod_count == out_mod_count:
+                    feed_title = f"{in_mod_count} feedback"
+                    feed_sig = Sig(0)
+                    self.routes[feed_title] = feed_sig
+                    #self.routes[feed_title].ctrl([SLMap(0, 8.0, 'lin', 'value', 0)], title=feed_title + " control")
+                    modded.modulate_phase(modding.output * ctrl_sig * feed_sig)
+
+                else:
+                    modded.modulate_phase(modding.output * ctrl_sig)
+
+                out_mod_count += 1
+            out_sig = Sig(0)
+            self.routes[f"{in_mod_count}out"] = out_sig
+            modding.out(out_sig)
+            #out_sig.ctrl([SLMap(0, 1.0, 'lin', 'value', 0)], title=f"{in_mod_count} output control")
+            in_mod_count += 1
 
     def noteon(self, freq, vel):
         self.vel.value = vel

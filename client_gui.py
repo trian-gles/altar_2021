c6b8232dfb627f9328cca53b39a7eaf0d1694a3f
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-05-09 21:54:03 -0400

    Added resources

--- client_gui.py
+++ client_gui.py
@@ -1,1 +1,15 @@
-import pygame
+import pygame as pg
+
+WIDTH = 1920
+HEIGHT = 1080
+
+BLACK = (55, 55, 55)
+WHITE = (255, 255, 255)
+LIGHT_GREY = (191, 191, 191)
+RED = (255, 0, 0)
+
+pg.init()
+
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+FONT = pg.font.Font('../resources/JetBrainsMono-Medium.ttf', 12)
+pg.display.set_caption("DX7 testing GUI")

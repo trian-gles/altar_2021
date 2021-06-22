0d388e3daeb4652f89537b1f1b755cdb77ea0aaa
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-06-21 23:36:14 -0400

    AudioManager test

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -3,13 +3,23 @@ import unittest
 
 from pyo import *
 from audio.dx7 import DX7Poly, DX7Mono, DXSineModule
-from audio.audiomanager import AudioManager, Zone
+from audio.audiomanager import Zone, AudioManager
 from audio.audio_cards import ALL_CARDS
 
-s = Server().boot()
+
+class AudioTest(unittest.TestCase):
+    s = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Server().boot()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.s.shutdown()
 
 
-class TestDX7Poly(unittest.TestCase):
+class TestDX7Poly(AudioTest):
     def setUp(self):
         self.synth = DX7Poly(4)
 
@@ -30,7 +40,7 @@ class TestDX7Poly(unittest.TestCase):
         self.assertEqual(self.synth.active_voice, self.synth.voices[1])
 
 
-class TestDX7Mono(unittest.TestCase):
+class TestDX7Mono(AudioTest):
     def setUp(self):
         self.synth = DX7Mono()
 
@@ -39,12 +49,12 @@ class TestDX7Mono(unittest.TestCase):
         self.assertEqual(self.synth.algo_num, 5)
 
 
-class TestDX7Sine(unittest.TestCase):
+class TestDX7Sine(AudioTest):
     def setUp(self):
         self.synth = DXSineModule(0)
 
 
-class TestZone(unittest.TestCase):
+class TestZone(AudioTest):
     def setUp(self):
         self.zone = Zone(0)
 
@@ -67,6 +77,24 @@ class TestZone(unittest.TestCase):
         self.assertEqual(self.zone.check_status()[0:2], ('atonal', 'static'))
         self.zone.input((19, None, None))
         self.assertEqual(self.zone.check_status()[1], 'quiet')
+
+
+class TestAM(AudioTest):
+    basic_hand = (0, None, None)
+
+    def setUp(self):
+        self.am = AudioManager()
+
+    def test_random_and_tonal(self):
+        self.am.input(((21, None, None), self.basic_hand, self.basic_hand))
+        self.am.input(((22, None, None), self.basic_hand, self.basic_hand))
+        for tup in self.am.check_status():
+            self.assertEqual(tup[0], "tonal")
+
+    def test_tree_card(self):
+        self.am.input(((26, None, None), self.basic_hand, self.basic_hand))
+        desired_pat = [48, None, None, None, 51, None, None, 55, None, None, None, 56, None, None, None, 51, None, 58]
+        self.assertEqual(self.am.zones[0].glob_pattern, desired_pat)
 
 
 if __name__ == "__main__":
